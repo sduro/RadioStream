8565ef1abd35c84b43465c42f95cea9263503b77
sduro <sduro@Asp5742>
2015-03-04 15:35:54 +0100

    update buttons

--- buttons/__init__.py
+++ buttons/__init__.py
@@ -1,15 +1,1 @@
-import time
-import RPi.GPIO as GPIO
-
-PLAY = 2
-REW = 3
-FOW = 4
-
-def press():
-    if GPIO.input(PLAY):
-        return 1
-    if GPIO.input(REW):
-        return 2
-    if GPIO.input(FOW):
-        return 3
-    
+ 
--- buttons/buttonsgpio.py
+++ buttons/buttonsgpio.py
@@ -1,3 +1,6 @@
+#Fichero de configuracion de los botones del GPIO
+#Para cambiar las emisoras
+
 import time
 import RPi.GPIO as GPIO
 
@@ -7,12 +10,12 @@ FOW = 4
 
 def buttons_init():
     GPIO.setmode(GPIO.BCM)     # Use BCM GPIO numbers
-    GPIO.setup(PLAY, GPIO.IN) # E
-    GPIO.setup(REW, GPIO.IN)  # RS
-    GPIO.setup(FOW, GPIO.IN)  # DB4
-   
-        
+    GPIO.setup(PLAY, GPIO.IN,pull_up_down=GPIO.PUD_DOWN) # play
+    GPIO.setup(REW, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)  # rew
+    GPIO.setup(FOW, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)  # fow
+      
 def press():
+    
     if GPIO.input(PLAY):
         return 1
     if GPIO.input(REW):
